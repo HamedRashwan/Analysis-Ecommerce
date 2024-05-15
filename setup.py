edbaf0162e9131cbd7583b686666b890212e3d7a
HamedRashwan <hamedrashwan2003@gmail.com>
2024-05-15 06:22:44 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='cleaning and Analyze ecommerce dataset and provide dashboard using dash that explore and support your analytics and Modeling Regession same SVR , KNN , lR and Saving this models',
+    author='Hamed Hamdy',
+    license='',
+)
